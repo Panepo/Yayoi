8c1116733b405039dc88bc57b7ad921a7e110d9a
Panepo <whisper.ko@gmail.com>
2018-07-12 17:49:06 +0800

    update notebook
    update notebook

--- network/srcnn.py
+++ network/srcnn.py
@@ -1,30 +1,33 @@
 from keras.models import Sequential
-from keras.layers import Conv2D
+from keras.layers import Conv2D, BatchNormalization
 from keras.optimizers import Adam
 
-def networkTrain():
-  network = Sequential()
-  network.add(Conv2D(nb_filter=128, nb_row=9, nb_col=9, init='glorot_uniform',
-                   activation='relu', border_mode='valid', bias=True, input_shape=(32, 32, 1)))
-  network.add(Conv2D(nb_filter=64, nb_row=3, nb_col=3, init='glorot_uniform',
-                   activation='relu', border_mode='same', bias=True))
-  network.add(Conv2D(nb_filter=1, nb_row=5, nb_col=5, init='glorot_uniform',
-                   activation='linear', border_mode='valid', bias=True))
-  adam = Adam(lr=0.0003)
-  network.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
-  return network
+class srcnn:
+  def __init__(self):
+    self.layers = 3
+    self.filter = [64, 32, 1]
+    self.conv = [9, 3, 5]
+    self.inputShape = (32, 32, 1)
+    self.lr = 0.0003
+    self.in_train = False
+
+  def network(self):
+    network = Sequential()
+
+    if (self.in_train):
+      network.add(Conv2D(nb_filter=self.filter[0], nb_row=self.conv[0], nb_col=self.conv[0], init='glorot_uniform',
+                         activation='relu', border_mode='valid', bias=True, input_shape=self.inputShape))
+    else:
+      network.add(Conv2D(nb_filter=self.filter[0], nb_row=self.conv[0], nb_col=self.conv[0], init='glorot_uniform',
+                         activation='relu', border_mode='valid', bias=True, input_shape=(None, None, 1)))
     
-def networkInference():
-  network = Sequential()
-  network.add(Conv2D(nb_filter=128, nb_row=9, nb_col=9, init='glorot_uniform',
-                   activation='relu', border_mode='valid', bias=True, input_shape=(None, None, 1)))
-  network.add(Conv2D(nb_filter=64, nb_row=3, nb_col=3, init='glorot_uniform',
-                   activation='relu', border_mode='same', bias=True))
-  network.add(Conv2D(nb_filter=1, nb_row=5, nb_col=5, init='glorot_uniform',
-                   activation='linear', border_mode='valid', bias=True))
-  adam = Adam(lr=0.0003)
-  network.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
-  return network
-    
-if __name__ == "__main__":
-  print("Hello world!")
+    for i in range(1, self.layers-1):
+      network.add(Conv2D(nb_filter=self.filter[i], nb_row=self.conv[i], nb_col=self.conv[i], init='glorot_uniform',
+                         activation='relu', border_mode='same', bias=True))
+      network.add(BatchNormalization())
+
+    network.add(Conv2D(nb_filter=self.filter[self.layers-1], nb_row=self.conv[self.layers-1], nb_col=self.conv[self.layers-1], init='glorot_uniform',
+                       activation='linear', border_mode='valid', bias=True))
+    adam = Adam(lr=self.lr)
+    network.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
+    return network
