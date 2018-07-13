755917c28e3df57f8d3f287c98372f41580223e4
Panepo <whisper.ko@gmail.com>
2018-07-13 14:22:05 +0800

    update notebook
    update notebook

--- network/srcnn.py
+++ network/srcnn.py
@@ -6,7 +6,7 @@ class srcnn:
   def __init__(self):
     self.layers = 3
     self.filter = [64, 32, 1]
-    self.conv = [9, 3, 5]
+    self.kernelSize = [(9,9), (3,3), (5,5)]
     self.inputShape = (32, 32, 1)
     self.lr = 0.0003
     self.in_train = False
@@ -15,18 +15,18 @@ class srcnn:
     network = Sequential()
 
     if (self.in_train):
-      network.add(Conv2D(filters=self.filter[0], nb_row=self.conv[0], nb_col=self.conv[0], kernel_initializer='glorot_uniform',
+      network.add(Conv2D(filters=self.filter[0], kernel_size = self.kernelSize[0], kernel_initializer='glorot_uniform',
                          activation='relu', padding='valid', use_bias=True, input_shape=self.inputShape))
     else:
-      network.add(Conv2D(filters=self.filter[0], nb_row=self.conv[0], nb_col=self.conv[0], kernel_initializer='glorot_uniform',
+      network.add(Conv2D(filters=self.filter[0], kernel_size = self.kernelSize[0], kernel_initializer='glorot_uniform',
                          activation='relu', padding='valid', use_bias=True, input_shape=(None, None, 1)))
     
     for i in range(1, self.layers-1):
-      network.add(Conv2D(filters=self.filter[i], nb_row=self.conv[i], nb_col=self.conv[i], kernel_initializer='glorot_uniform',
+      network.add(Conv2D(filters=self.filter[i], kernel_size = self.kernelSize[i], kernel_initializer='glorot_uniform',
                          activation='relu', padding='same', use_bias=True))
       #network.add(BatchNormalization())
 
-    network.add(Conv2D(filters=self.filter[self.layers-1], nb_row=self.conv[self.layers-1], nb_col=self.conv[self.layers-1], kernel_initializer='glorot_uniform',
+    network.add(Conv2D(filters=self.filter[self.layers-1], kernel_size = self.kernelSize[self.layers-1], kernel_initializer='glorot_uniform',
                        activation='linear', padding='valid', use_bias=True))
     adam = Adam(lr=self.lr)
     network.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
--- network/waifu2x.py
+++ network/waifu2x.py
@@ -6,9 +6,8 @@ from keras.optimizers import Adam
 class waifu2x:
   def __init__(self):
     self.layers = 7
-    self.nOutputPlane = [32, 32, 64, 64, 128, 128, 1]
-    self.kH = [3, 3, 3, 3, 3, 3, 3]
-    self.kW = [3, 3, 3, 3, 3, 3, 3]
+    self.filters = [32, 32, 64, 64, 128, 128, 1]
+    self.kernelSize = [(3,3), (3,3), (3,3), (3,3), (3,3), (3,3), (3,3)]
     self.inputShape = (32, 32, 1)
     self.lr = 0.0003
     self.in_train = False
@@ -18,29 +17,33 @@ class waifu2x:
 
     if (self.in_train):
       network.add(Conv2D(
-        self.nOutputPlane[0], 
-        (self.kH[0],self.kW[0]),
+        filters = self.filters[0], 
+        kernel_size = self.kernelSize[0],
         kernel_initializer='zero',
         padding='same', 
         use_bias=True, 
         input_shape=self.inputShape))
     else:
       network.add(Conv2D(
-        self.nOutputPlane[0], 
-        (self.kH[0],self.kW[0]),
+        filters = self.filters[0], 
+        kernel_size = self.kernelSize[0],
         kernel_initializer='zero',
         padding='same', 
         use_bias=True, 
-        input_shape=self.inputShape))
+        input_shape=(None, None, 1)))
     
     network.add(LeakyReLU(0.1))
     for i in range(1, self.layers-1):
       network.add(Conv2D(
-        self.nOutputPlane[i], 
-        (self.kH[i],self.kW[i]),
+        filters = self.filters[i], 
+        kernel_size = self.kernelSize[i],
         kernel_initializer='zero',
         padding='same', 
         use_bias=True))
       network.add(LeakyReLU(0.1))
 
+    network.add(Conv2D(filters=self.filters[self.layers-1], kernel_size = self.kernelSize[self.layers-1], kernel_initializer='zero',
+                       padding='same', use_bias=True))
+    adam = Adam(lr=self.lr)
+    network.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
     return network
