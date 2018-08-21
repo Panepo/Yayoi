ee0ee62bb54fb33654c5e03b51dbd75e242176ff
Panepo <whisper.ko@gmail.com>
2018-08-21 09:52:41 +0800

    update notebook
    update notebook

--- dockerCpData.bat
+++ dockerCpData.bat
@@ -3,6 +3,8 @@ docker cp ./model %1:/home/jovyan/work
 docker cp ./checkpoint %1:/home/jovyan/work
 
 docker cp ./imageTrain %1:/home/jovyan/work
+docker cp ./imageTrain2 %1:/home/jovyan/work
 docker cp ./imageValidate %1:/home/jovyan/work
+docker cp ./imageValidate2 %1:/home/jovyan/work
 
 docker cp ./utility %1:/home/jovyan/work
